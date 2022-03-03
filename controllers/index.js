a157db3a88860aeb59605b83ec3e28e7b117fef9
bosen L <bosenli2013@hotmail.com>
2022-03-02 23:49:31 -0500

    added CRUD for postman testing

--- controllers/index.js
+++ controllers/index.js
@@ -15,7 +15,7 @@ async function createPlant(req, res) {
   }
 }
 
-async function getAllPlants() {
+async function getAllPlants(req,res) {
   try {
     const plants = await Plant.find()
     return res.status(200).json({plants})
@@ -24,7 +24,74 @@ async function getAllPlants() {
   }
 }
 
+
+async function getPlantById(req, res) {
+    try {
+      const { id } = req.params;
+      const plant = await Plant.findById(id)
+      if (plant) {
+        return res.status(200).json({plant})
+      }
+      return res.status(404).send('Plant with the specified ID does not exist');
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
+
+//   const updatePlant = async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         await Plant.findByIdAndUpdate(id, req.body, { new: true }, (err, plant) => {  //{property}
+//             if (err) {
+//                 res.status(500).send(err);
+//             }
+//             if (!plant) { //if there is no plant
+//                 res.status(500).send('Plant not found!');
+//             }
+//           return res.status(200).json({ plant });
+//         })
+//     } catch (error) {
+//         return res.status(500).send(error.message);
+//     }
+//   }
+  
+const updatePlant =  (req, res) => {
+    try {
+      const { id } = req.params;
+     Plant.findByIdAndUpdate(id, req.body, { new: true }, function(err, plant) { // cant use arrow function
+       
+        if (err !== null) {
+          console.log(err, 'error')
+          res.status(404).send(err.message)
+        } else {
+          console.log(plant)
+          res.json(plant)
+        }
+      })
+    } catch (error) {
+     return  res.status(500).send(error.message)
+    }
+  }
+
+  async function deletePlant(req, res) {
+    try {
+      const { id } = req.params;
+      const deleted = await Plant.findByIdAndDelete(id)
+      if (deleted) {
+        return res.status(200).send("Plant deleted");
+      }
+      throw new Error("Plant not found");
+    } catch (error) {
+      return res.status(500).send(error.message);
+    }
+  }
+  
+
+//basically export to "routes" file
 module.exports = {
   createPlant, 
-  getAllPlants
+  getAllPlants,
+  getPlantById,
+  updatePlant,
+  deletePlant
 }
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,18 @@ const router = Router();
 router.get('/', (req, res) => res.send('this is the root!'))
 
 router.post('/plants', controllers.createPlant)  //it is from controller
+//in post man body test on json
+// {
+//     "name": "Test Plant",
+//     "description": "Test Deciption",
+//     "image": "https://testimage.com/plant.png"
+// }
+
+router.get('/plants', controllers.getAllPlants)
+
+router.get('/plants/:id', controllers.getPlantById) //add JSON Viewer extension to google chrome
+
+router.put('/plants/:id', controllers.updatePlant)
+
 
 module.exports = router; 
